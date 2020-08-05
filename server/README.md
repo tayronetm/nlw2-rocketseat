1bf33c1722a1eb54d9cac33d694235171b9963e0
Tayrone <tayronetnm@gmail.com>
2020-08-05 08:19:07 -0300

    Migrations sql, routes, params, filters api

---
+++ server/README.md
@@ -0,0 +1,17 @@
+# Dependências 
+
+- knex (Facilita consultas sql, criar migrations, tabelas...)
+- sqlite3 (Banco de dados SQL)
+
+
+# Funcionalidades
+
+## Conexões
+
+- Rota para listagem do total de conexões realizadas;
+- Criar uma nova conexão;
+
+## Aulas
+- Rota para criar uma aula;
+- Listar aulas;
+	- Filtrar por matéria, dia da semana e horário
---
+++ server/src/routes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import ClassController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesController = new ClassController();
+const connectionsController = new ConnectionsController();
+
+// GET: Buscar uma informação
+// POST: Criar uma nova informação
+// PUT: Atualizar uma informação existente
+// DELETE: Deletar uma informação
+
+// ** POR PADRÃO O NAVEGADOR EXECUTA O GET SE NÃO ACHAR EXIBE UM "CANNOT GET /"
+
+// PARAMETROS
+// 1. Corpo (request.body): Dados para criação ou atualização de uma entidade;
+// 2. Route Params(request.params) : Identificar qual recurso eu quero atualizar ou deletar; /:id
+// 3. Query Params(request.query): Páginação, filtros, ordenação...
+
+routes.get('/classes', classesController.list);
+routes.post('/classes', classesController.create);
+
+routes.get('connections', connectionsController.list)
+routes.post('connections', connectionsController.create)
+
+export default routes;
