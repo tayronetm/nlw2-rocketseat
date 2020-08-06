ed07ffb1bd4bb99b5da0d92abd032e057f95674c
Tayrone <tayronetnm@gmail.com>
2020-08-06 07:56:44 -0300

    Integration API

--- web/src/pages/home/home.tsx
+++ web/src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import  { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -8,8 +8,18 @@ import giveClassIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './home-style.css'
+import api from '../../services/api';
 
 function Landing() {
+	const [ totalConnections, setTotalConnections ] = useState(0);
+	
+	useEffect(() => {
+		api.get('connections').then(response => {
+			const total = response.data;
+			setTotalConnections(total)
+		})
+	}, []);
+	
 	return (
 		<div id="page-landing">
 			<div id="page-landing-content" className="container">
@@ -35,7 +45,7 @@ function Landing() {
 					</Link>
 				</div>
 				<span className="total-connections">
-					Total de 200 conexões já realizadas. <img src={purpleHeartIcon} alt="Coração roxo"></img>
+					Total de {totalConnections} conexões já realizadas. <img src={purpleHeartIcon} alt="Coração roxo"></img>
 				</span>
 			</div>
 		</div>
